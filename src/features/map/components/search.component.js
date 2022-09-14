c3dd0b2244053f35e550b931f9b2cfdf0a6d32b9
Ivaylo <ivayloivan0v@icloud.com>
2022-09-15 00:40:45 +0300

    setting the map region

--- src/features/map/components/search.component.js
+++ src/features/map/components/search.component.js
@@ -1,16 +1,19 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styled from "styled-components/native";
 import { Searchbar } from "react-native-paper";
 import { LocationContext } from "../../../services/location/location.context";
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
+  position: absolute;
+  z-index: 999;
+  top: 40px;
+  width: 100%;
 `;
 
 export const Search = () => {
   const { keyword, search } = useContext(LocationContext);
   const [searchKeyword, setSearchKeyword] = useState(keyword);
-
   useEffect(() => {
     setSearchKeyword(keyword);
   }, [keyword]);
@@ -19,6 +22,7 @@ export const Search = () => {
     <SearchContainer>
       <Searchbar
         placeholder="Search"
+        icon="map"
         value={searchKeyword}
         onSubmitEditing={() => {
           search(searchKeyword);
