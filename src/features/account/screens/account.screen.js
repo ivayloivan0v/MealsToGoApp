c39386054f80d34b9464e5d718c3dfe4a085f080
Ivaylo <ivayloivan0v@icloud.com>
2022-09-16 04:54:03 +0300

    adding account screen buttons

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,14 +1,37 @@
 import React from "react";
 
+import { Spacer } from "../../../components/spacer/spacer.component";
+
 import {
   AccountBackground,
+  AccountContainer,
   AccountCover,
+  AuthButton,
 } from "../components/authentication.styles";
 
-export const AccountScreen = () => {
+export const AccountScreen = ({ navigation }) => {
   return (
     <AccountBackground>
       <AccountCover />
+      <AccountContainer>
+        <AuthButton
+          icon="lock-open-outline"
+          mode="contained"
+          onPress={() => navigation.navigate("Login")}
+        >
+          Login
+        </AuthButton>
+        <Spacer size="large">
+          <AuthButton
+            icon="lock-open-outline"
+            color="black"
+            mode="contained"
+            onPress={() => navigation.navigate("Register")}
+          >
+            Register
+          </AuthButton>
+        </Spacer>
+      </AccountContainer>
     </AccountBackground>
   );
 };
