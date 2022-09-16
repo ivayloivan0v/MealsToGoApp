f8bcaf1259e295858345475008073d93a55762b5
Ivaylo <ivayloivan0v@icloud.com>
2022-09-17 01:56:35 +0300

    adding a watermelon animation to the account screen

--- src/components/utility/safe-area.component.js
+++ src/components/utility/safe-area.component.js
@@ -3,5 +3,6 @@ import { StatusBar, SafeAreaView } from "react-native";
 
 export const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`}
+  background-color: ${(props) => props.theme.colors.bg.primary}
 `;
